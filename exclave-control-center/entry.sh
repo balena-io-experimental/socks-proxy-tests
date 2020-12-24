99a08b2991da7df375abdeccf06f011ee9256823
Vipul Gupta (@vipulgupta2048) <vipul@balena.io>
2020-12-24 17:04:40 +0530

    First build
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipul@balena.io>

--- exclave-control-center/entry.sh
+++ exclave-control-center/entry.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-
 rm  -R /var/run/docker
 rm /var/run/docker.sock
 
 set -eu
+# rc-update add docker boot.
 
 # https://docs.docker.com/engine/reference/commandline/dockerd/
 dockerd &
 
 sleep 5
 
-# cargo run -- -c /usr/src/core/tests
+# service docker start
 
+# cargo run -- -c /usr/src/core/tests
 tail -f /dev/null
