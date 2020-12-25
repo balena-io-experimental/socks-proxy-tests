8dbe4c76880f38d85cb2782fb5ef351d236e461f
Vipul Gupta (@vipulgupta2048) <vipul@balena.io>
2020-12-25 11:17:16 +0530

    Add *ignore files
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipul@balena.io>

--- exclave-control-center/entry.sh
+++ exclave-control-center/entry.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-rm  -R /var/run/docker
+rm -R /var/run/docker
 rm /var/run/docker.sock
 
 set -eu
-# rc-update add docker boot.
 
 # https://docs.docker.com/engine/reference/commandline/dockerd/
-dockerd &
-
-sleep 5
-
-# service docker start
+dockerd & sleep 5
 
 # cargo run -- -c /usr/src/core/tests
 tail -f /dev/null
+
+# Alpine specific Docker commands
+# service docker start
+# rc-update add docker boot.
