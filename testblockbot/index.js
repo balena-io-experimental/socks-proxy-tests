4b287b13cd9176d9095d909f208ddd1379ace916
Vipul Gupta (@vipulgupta2048) <vipul@balena.io>
2021-01-12 12:58:35 +0530

    Add more logging to testblockbot
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipul@balena.io>

--- testblockbot/index.js
+++ testblockbot/index.js
@@ -9,39 +9,43 @@ const app = express()
 const SERVER_PORT = 5001
 
 app.get('/', (req, res) => {
-  return res.status(200).send('OK');
+  console.log(`Testblockbot listening at testblockbot:${SERVER_PORT}`);
+  return res.status(200).send('OK \n');
 });
 
 app.get('/ping', (req, res) => {
-  console.log(`TestbotSDK-block listening at testblockbot:${SERVER_PORT}`);
-  return res.status(200).send('OK');
+  console.log(`Testblockbot listening at testblockbot:${SERVER_PORT}`);
+  return res.status(200).send('OK \n');
 });
 
-app.listen(SERVER_PORT, asyncHandler( async() => {
+app.listen(SERVER_PORT, asyncHandler(async () => {
   await testbot.setup()
-  console.log(`TestbotSDK-block listening at testblockbot:${SERVER_PORT}`);
+  console.log(`Testblockbot listening at testblockbot:${SERVER_PORT}`);
 }));
 
 app.get('/on', asyncHandler(async (req, res) => {
   await deviceInteractor.powerOn()
-  return res.status(200).send("Okay Powering On now")
+  return res.status(200).send("Powering on DUT now \n")
 }))
 
 app.get('/setup', asyncHandler(async (req, res) => {
   await testbot.setup()
-  return res.status(200).send("Okay Setting up the DUT now")
+  return res.status(200).send("Setting up the DUT \n")
 }))
 
 app.get('/teardown', asyncHandler(async (req, res) => {
   await testbot.teardown()
+  return res.status(200).send("Completed resetting the hub \n")
 }))
 
 app.get('/off', asyncHandler(async (req, res) => {
   await deviceInteractor.powerOff()
+  return res.status(200).send("Powering off DUT now \n")
 }))
 
 app.get('/flash', asyncHandler(async (req, res) => {
   await deviceInteractor.flashFromFile(req.query.path);
+  return res.status(200).send(`DUT has been flashed with ${req.query.path} \n`)
 }))
 
 // Functionality coming soon in a block near you
