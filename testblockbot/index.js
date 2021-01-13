b276c149b8258743314b5152b23b4454029ec508
Vipul Gupta (@vipulgupta2048) <vipul@balena.io>
2021-01-14 01:51:05 +0530

    Add util-linux to testblockbot
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipul@balena.io>

--- testblockbot/index.js
+++ testblockbot/index.js
@@ -44,8 +44,8 @@ app.get('/off', asyncHandler(async (req, res) => {
 }))
 
 app.get('/flash', asyncHandler(async (req, res) => {
-  await deviceInteractor.flashFromFile("/images/raspberrypi3.img")
-  res.status(200).send("Flashing Complete")
+  await deviceInteractor.flashFromFile(req.query.path)
+  return res.status(200).send("Flashing Complete")
 }))
 
 // Functionality coming soon in a block near you
