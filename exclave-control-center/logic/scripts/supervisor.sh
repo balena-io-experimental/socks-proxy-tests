36afbf02b8de626239efdb49053544069d7d6f9e
Vipul Gupta (@vipulgupta2048) <vipul@balena.io>
2021-01-14 02:31:26 +0530

    Add JSON string generation with jq
    Signed-off-by: Vipul Gupta (@vipulgupta2048) <vipul@balena.io>

--- exclave-control-center/logic/scripts/supervisor.sh
+++ exclave-control-center/logic/scripts/supervisor.sh
@@ -1,27 +1,18 @@
-#  curl -X POST --header "Content-Type:application/json" \
-#     --header "authorization: Bearer ${API_KEY}" \
-#     --data '{"uuid": "${DEVICE_UUID}", "method": "GET"}' \
-#     "https://api.balena-cloud.com/supervisor/v1/device/host-config"
+#!/bin/bash
+
+SET_HOST_CONFIG=$( jq -n --arg du "$DEVICE_UUID" \
+             '{ "uuid": $du, "method": "PATCH", "data": { "network": { "proxy": { "type": "socks5", "ip": "10.42.0.249", "port": 8123}, "hostname": "" }}}' )
 
 curl --request POST \
-    --url https://api.balena-cloud.com/supervisor/v1/device/host-config \
+     --url https://api.balena-cloud.com/supervisor/v1/device/host-config \
+     --header "authorization: Bearer ${API_KEY}" \
+     --header 'content-type: application/json' \
+     --data "${SET_HOST_CONFIG}"
+
+GET_HOST_CONFIG=$( jq -n --arg du "$DEVICE_UUID" '{ "uuid": $du, "method": "GET" }')
+echo "Host-config configuration on device:"
+
+curl -X POST --header "Content-Type:application/json" \
     --header "authorization: Bearer ${API_KEY}" \
-    --header 'content-type: application/json' \
-    --data '{
-        "uuid": "${DEVICE_UUID}", 
-        "method": "PATCH", 
-        "data": {
-            "network": { 
-                "proxy": {
-                    "type": "socks5",
-                    "ip": "10.42.0.249",
-                    "port": 8123,
-                    "login": "",
-                    "password": "",
-                    "noProxy": []
-                },
-                "hostname": ""
-                }
-            }
-        }
-    }'
+    --data "${GET_HOST_CONFIG}" \
+    "https://api.balena-cloud.com/supervisor/v1/device/host-config" | jq 
